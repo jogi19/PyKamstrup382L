94afe0cb49ea417677442cd9342289904cee8eca
Jochen Weiss <mail2jogi@googlemail.com>
2021-01-10 10:57:07 +0100

    save cuurent state

--- src/create_statistics_daily.py
+++ src/create_statistics_daily.py
@@ -3,9 +3,6 @@ import mysql.connector
 import datetime
 from datetime import *
 
-start_value_serial=16721739
-    
-
 def create_connection(db_file):
     """ 
     create a database connection energy_db
@@ -36,15 +33,27 @@ def read_from_data(mysqldb,serialnumber,starttime=0,start_value_positive_active_
         #print(str(row))
         #print(row[0])
         #print(row[4])
-        if(row[4]>start_value_positive_active_energie):
-            average_power =calculate_average_power(start_value_positive_active_energie, row[4], start_value_time,str(row[0]))
+        xtime1 = datetime.strptime(start_value_time,"%Y-%m-%d %H:%M:%S")
+        xtime2 = datetime.strptime(str(row[0]),"%Y-%m-%d %H:%M:%S")
+        
+        xdelta = xtime2.date() - xtime1.date()
+        if(xdelta.days>0):
+            print(str(xtime1))
+            print(str(xtime2))
+            print(str(xdelta.days))
+            xdelta_energy=float(row[4])-start_value_positive_active_energie
+            print(start_value_positive_active_energie)
+            print(row[4])
+            print(xdelta_energy)
+            start_value_positive_active_energie=row[4]
+            start_value_time=str(row[0])
             print("to be insert into database:")
             print("timer: "+str(row[0]))
             print("timerzone: "+str(row[1]))
             print("electric_meter_type: "+str(row[2]))
             print("serial_no: "+str(serialnumber))
             print("value_positive_active: "+str(row[4]))
-            print("average power: "+ str(average_power))
+            print("daily_active_energie: "+ str(xdelta_energy))
             #muss wieder raus, wenn es auf der selben Database läuft
             localsqldb = mysql.connector.connect(
                 host="localhost",
@@ -54,13 +63,13 @@ def read_from_data(mysqldb,serialnumber,starttime=0,start_value_positive_active_
             )
             localsqldb.database = "energy_db"
             localcursor = localsqldb.cursor()
-            sql = "INSERT INTO average_power_in(\
+            sql = "INSERT INTO daily_power(\
                     time,\
                     timezone,\
                     electric_meter_type,\
                     serial_number,\
                     positive_active_energie,\
-                    average_active_energie)\
+                    daily_active_energie)\
                     VALUES \
                     ( \
                     \'"+str(row[0])+"\',\
@@ -68,26 +77,11 @@ def read_from_data(mysqldb,serialnumber,starttime=0,start_value_positive_active_
                     \'"+str(row[2])+"\',\
                     "+str(serialnumber)+",\
                     "+str(row[4])+",\
-                    "+str(average_power)+");"
+                    "+str(xdelta_energy)+");"
             print(sql)
             localcursor.execute(sql)
             localsqldb.commit()
-            start_value_positive_active_energie=row[4]
-            start_value_time=str(row[0])
 
-def calculate_average_power(old_P, new_P, old_time,new_time):
-    print("########## calculate_average_power #############")
-    print("old_P: "+str(old_P))
-    print("new_P: "+str(new_P))
-    print("old_time: "+old_time)
-    print("new_time: "+new_time)
-    time1 = datetime.strptime(old_time,"%Y-%m-%d %H:%M:%S")
-    time2 = datetime.strptime(new_time,"%Y-%m-%d %H:%M:%S")
-    delta=time2-time1
-    print("Delta: "+str(delta.seconds))
-    average_power=(new_P-old_P)*3600*1000/(delta.seconds)
-    print("average_power: "+ str(average_power))
-    return average_power
 
 def get_start_values_energy(serialnumber):
     positive_active_energie = 0
@@ -132,17 +126,18 @@ def get_start_values_time(serialnumber):
 
 connection = create_connection("energy_db")
 # first start ....
-#read_from_data(connection,16721739,"2020-12-11 21:50:30",35936.0)
-#read_from_data(connection,17717664,"2020-12-11 21:47:46",17807.0)
+read_from_data(connection,16721739,"2020-12-11 21:50:30",35936.0)
+read_from_data(connection,17717664,"2020-12-11 21:47:46",17807.0)
 
 # after initial start use
-start_values_time = get_start_values_time(16721739)
-start_values_energy = get_start_values_energy(16721739)
-read_from_data(connection,16721739,str(start_values_time),float(start_values_energy))
+#start_values_time = get_start_values_time(16721739)
+#start_values_energy = get_start_values_energy(16721739)
+#read_from_data(connection,16721739,str(start_values_time),float(start_values_energy))
 
-start_values_time = get_start_values_time(17717664)
-start_values_energy = get_start_values_energy(17717664)
-read_from_data(connection,17717664,str(start_values_time),float(start_values_energy))
+#start_values_time = get_start_values_time(17717664)
+#start_values_energy = get_start_values_energy(17717664)
+#read_from_data(connection,17717664,str(start_values_time),float(start_values_energy))
 
 
 
+17717664
---
+++ src/create_table_daily_power.py
@@ -0,0 +1,51 @@
+'''
+source: https://www.w3schools.com/python/python_mysql_getstarted.asp
+This code was just to leran how to handle SQL data base with python
+and helped me to create the required databases and tables
+I'm not sure if this version creates the correct databases
+So, if you find issues, don't hessitate to push fixes :-)
+'''
+
+import mysql.connector
+
+def create_database(database):
+  mycursor.execute("CREATE DATABASE " + database)
+
+def show_databases():
+  mycursor.execute("SHOW DATABASES")
+  for x in mycursor:
+    print(x)
+
+def create_table(database, table):
+  mydb.database = database
+  mycursor.execute("CREATE TABLE "+ table+"(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))")
+
+def create_weather_table(database, table):
+  mydb.database = database
+  sql = "CREATE TABLE "+ table+"( time DATETIME NOT NULL,\
+    timezone INT,\
+    electric_meter_type CHAR(64),\
+    serial_number INT,\
+    positive_active_energie FLOAT,\
+    daily_active_energie FLOAT,\
+    feeding_daily_active_energie FLOAT)"
+  print(sql)
+  mycursor.execute(sql)
+
+
+
+'''
+def alter_table(database, table):
+  mydb.database = database
+  mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY") 
+'''
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="strom",
+  password="power"
+)
+mycursor = mydb.cursor()
+#mycursor.execute("CREATE DATABASE energy_db")
+show_databases()
+create_weather_table("energy_db","daily_power")
+
